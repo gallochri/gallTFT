8eb717ec20b443eef665c6f378a0345d50cefa93
gallochri <chri@gallochri.com>
2017-09-24 21:28:39 +0200

    Support for Wemos D1 mini added

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,15 @@
 
 #include <SPI.h>
 #include "Ucglib.h"
+//PIN definition
+//Arduino Uno/Nano and Wemos D1 mini/pro
+#define SCLK  /*13*/    14  /*D5*/
+#define DATA  /*11*/    4   /*D2*/
+#define CD    /*9*/     16  /*D0*/
+#define CS    /*10*/    15  /*D8*/
+#define RESET /*8*/     2   /*D4*/
 
-Ucglib_ST7735_18x128x160_SWSPI ucg(/*sclk=*/ 13, /*data=*/ 11, /*cd=*/ 9 , /*cs=*/ 10, /*reset=*/ 8);
+Ucglib_ST7735_18x128x160_SWSPI ucg(SCLK,DATA,CD,CS,RESET);
 
 void setup(void)
 {
@@ -18,9 +25,9 @@ void loop(void) {
     ucg.setFont(ucg_font_ncenR08_tf);
     ucg.setColor(255, 255, 255);
 
-    ucg.setPrintPos(0, 15);
+    ucg.setPrintPos(5, 15);
     ucg.print("Hello World!");
-    ucg.setPrintPos(0,30);
+    ucg.setPrintPos(5,30);
     ucg.print("Second line");
 
     delay(500);
